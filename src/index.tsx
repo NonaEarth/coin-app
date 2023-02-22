0db5e1f541d49f1c52c8cf9236842bed79ad8741
NonaEarth <hsoj4129@gmail.com>
2023-02-22 14:53:33 +0900

    refactor: format imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { RecoilRoot } from "recoil";
-import App from "./App";
 
 const queryClient = new QueryClient();
 ReactDOM.render(
